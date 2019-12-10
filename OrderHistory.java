92f5e122ccd48d6e8637bf6e7fc3bcec2bdf3e5d
Calvin Luong <48378030+calvin-luong@users.noreply.github.com>
2019-12-09 17:44:21 -0800

    Add files via upload

--- OrderHistory.java
+++ OrderHistory.java
@@ -13,6 +13,8 @@ import java.awt.Font;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ScrollPaneConstants;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -43,7 +45,7 @@ public class OrderHistory extends JFrame {
 	 */
 	public OrderHistory() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 450);
+		setBounds(100, 100, 600, 725);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -54,7 +56,6 @@ public class OrderHistory extends JFrame {
 		
 		JTextPane textPane = new JTextPane();
 		textPane.setEditable(false);
-		JScrollPane jsp = new JScrollPane(textPane);
 		
 		for (Order o: Login.userList.get(pos).orders) {
 			String name = o.getName();
@@ -75,16 +76,16 @@ public class OrderHistory extends JFrame {
 		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(7)
-					.addComponent(textPane, GroupLayout.DEFAULT_SIZE, 577, Short.MAX_VALUE)
-					.addGap(6))
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
 					.addContainerGap(263, Short.MAX_VALUE)
 					.addComponent(btnBack)
 					.addGap(252))
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addGap(7)
+					.addComponent(textPane, GroupLayout.DEFAULT_SIZE, 577, Short.MAX_VALUE)
+					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -92,8 +93,8 @@ public class OrderHistory extends JFrame {
 					.addContainerGap()
 					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
-					.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 251, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+					.addComponent(textPane, GroupLayout.DEFAULT_SIZE, 554, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(btnBack)
 					.addGap(32))
 		);
